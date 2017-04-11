6768eb3c14b34437dcaf68b6d1b732614bd169aa
PandaRossi <rhamilton94@hotmail.co.uk>
2017-04-11 13:20:36 +0100

    First Commit
    Adding unity project to github

---
+++ Final FINAL/Assets/Scripts/TriggerdSuccess.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerdSuccess : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+
+
+
+        }
+        Debug.Log("Jumped Over Car");
+
+    }
+}
---
+++ Final FINAL/Assets/Scripts/TriggeredTone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggeredTone : MonoBehaviour {
+
+    public AudioClip tone;
+    private AudioSource source;
+
+    // Use this for initialization
+    void Start () {
+        source = GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            source.PlayOneShot(tone);
+            
+        }
+        Debug.Log("Tone Played");
+    }
+}
