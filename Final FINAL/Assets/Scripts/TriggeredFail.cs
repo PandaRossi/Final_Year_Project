b00e70be2f48a0351b3663e859dd82c6412d167d
Unknown <rhamilton94@hotmail.co.uk>
2017-04-11 13:34:39 +0100

    First Commit
    Adding the Unity Game onto GitHub

---
+++ Final FINAL/Assets/Scripts/TriggeredFail.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggeredFail : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+
+          
+
+        }
+        Debug.Log("Hit by Car");
+
+    }
+}
